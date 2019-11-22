f07ac62eee22a6166dfa9e4bf8cb50888b95a21c
eamaya2008 <43824734+eamaya2008@users.noreply.github.com>
2019-11-22 18:03:09 -0300

    Creacion Proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Soporte</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <header>
+      <img src="img/LogoCase2017Navidad.png" alt="LogoCase" id="LogoCaseHeader">
+      <h1>Detalle Tickets</h1>
+    </header>
+    <div class="main">
+      <div class="table-responsive">
+        <table class="tickets-cerrados-hoy">
+          <tbody>
+            <tr>
+              <th class="text-center">Hoy</th>
+            </tr>
+            <tr>
+              <th class="text-center">Abiertos</th>
+              <th class="text-center">Cerrados</th>
+            </tr>
+            <tr>
+              <th class="text-center">14</th>
+              <th class="text-center">25</th>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="tickets-cerrados-mes">
+          <tbody>
+            <tr>
+              <th class="text-center">Total de Tickets Abiertos</th>
+            </tr>
+            <tr>
+              <th class="text-center">Ayer</th>
+              <th class="text-center">Ultimos 7 dias</th>
+              <th class="text-center">Ultimos 30 dias</th>
+            </tr>
+            <tr>
+              <th class="text-center">14</th>
+              <th class="text-center">25</th>
+              <th class="text-center">30</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
