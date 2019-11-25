6c50e433d61a1b3598531d8c2fe4917dd6bd4454
eamaya2008 <eamaya2008@hotmail.com>
2019-11-24 21:57:38 -0300

    Vista ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,49 +3,100 @@
   <head>
     <meta charset="utf-8">
     <title>Soporte</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
     <header>
       <img src="img/LogoCase2017Navidad.png" alt="LogoCase" id="LogoCaseHeader">
-      <h1>Detalle Tickets</h1>
     </header>
     <div class="main">
-      <div class="table-responsive">
-        <table class="tickets-cerrados-hoy">
-          <tbody>
-            <tr>
-              <th class="text-center">Hoy</th>
-            </tr>
-            <tr>
-              <th class="text-center">Abiertos</th>
-              <th class="text-center">Cerrados</th>
-            </tr>
-            <tr>
-              <th class="text-center">14</th>
-              <th class="text-center">25</th>
-            </tr>
-          </tbody>
-        </table>
-
-        <table class="tickets-cerrados-mes">
-          <tbody>
-            <tr>
-              <th class="text-center">Total de Tickets Abiertos</th>
-            </tr>
-            <tr>
-              <th class="text-center">Ayer</th>
-              <th class="text-center">Ultimos 7 dias</th>
-              <th class="text-center">Ultimos 30 dias</th>
-            </tr>
-            <tr>
-              <th class="text-center">14</th>
-              <th class="text-center">25</th>
-              <th class="text-center">30</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <div class="row ticketCerrado">
+        <div class="col-md-2">
+          <img src="img/ItSupport.gif" alt="animed.gif" id="gif-sup">
+        </div>
+        <div class=" row ticketCerradoCounter col-md-10">
+        <div class="ticketCerradosDia col-md-1.5">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Hoy</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 tcd">Abiertos</div>
+            <div class="col-md-6 tcd">Cerrados</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 tcd">22</div>
+            <div class="col-md-6 tcd">33</div>
+          </div>
+        </div>
+        <div class="ticketCerradosUsu1 col-md-1.5">
+          <div class="row">
+            <div class="col-md-6 usu-name">Federico</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Pablo</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Matias</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Esteban</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Sebastian</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+        </div>
+        <div class="ticketCerradosUsu2 col-md-1.5">
+          <div class="row">
+            <div class="col-md-6 usu-name">Ivonne</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Rosana</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Patricia</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Emilce</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 usu-name">Belen</div>
+            <div class="col-md-6 usu-counter">150</div>
+          </div>
+        </div>
+        <div class="ticketAbiertosMes col-md-3">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Abiertos</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-8">Ayer</div>
+            <div class="col-md-4">125</div>
+          </div>
+          <div class="row">
+            <div class="col-md-8">Ultimos 7 dias</div>
+            <div class="col-md-4">125</div>
+          </div>
+          <div class="row">
+            <div class="col-md-8">Ultimos 30 dias</div>
+            <div class="col-md-4">125</div>
+          </div>
+          </div>
+          </div>
+        </div>
     </div>
   </body>
 </html>
